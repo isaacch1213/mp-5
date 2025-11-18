e123b9f1511b802246bd4064ae02ab885cfc028a
Isaac Chan <isaacch@bu.edu>
2025-11-18 03:04:13 -0500

    added metadata title

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from 'next';
 import Home from "@/components/Home";
+
+export const metadata: Metadata = {
+  title: "Home | URL Shortener",
+};
 
 export default function HomePage() {
   return (

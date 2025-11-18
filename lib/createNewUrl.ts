db5efc4ee42d1dc0354bb71e76ade26c4dc922ed
Isaac Chan <isaacch@bu.edu>
2025-11-18 03:02:58 -0500

    add check for alias

--- lib/createNewUrl.ts
+++ lib/createNewUrl.ts
@@ -12,7 +12,7 @@ export default async function createNewUrl(
 
     // Updated all error return statements to be an object, as Next.js does not render
     // error statements
-    
+
     // Checks if URL is valid
     if (!validateLongUrl(longUrl)) {
         return { error: "Invalid URL. Must start with https:// or http://, be a valid website, and contain no spaces." };
@@ -21,6 +21,11 @@ export default async function createNewUrl(
     // Checks if alias is valid
     if (alias.includes(" ")) {
         return { error: "Alias cannot contain spaces" };
+    }
+
+    // Checks for characters that are not safe in a URL path segment
+    if (encodeURIComponent(alias) !== alias) {
+        return { error: "Invalid alias: You may only use valid URL characters." };
     }
 
     const urlCollection = await getCollection(URL_COLLECTION);
